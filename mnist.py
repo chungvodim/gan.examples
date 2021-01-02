40e72943c5e4fefec568ce10903f89e02046667e
long.nguyen@orientsoftware.net <Ltg87861$>
2021-01-02 15:10:20 +0700

    Add requirements

---
+++ mnist.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import glob
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import PIL
+from tensorflow.keras import layers
+import time
+
+from IPython import display
